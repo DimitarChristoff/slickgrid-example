c4ab6aeefb649ae59f9542427a9def8fb153369d
DimitarChristoff <christoff@gmail.com>
2018-05-09 12:39:26 +0100

    initial build

---
+++ src/App.js
@@ -0,0 +1,320 @@
+import React                                                      from 'react';
+import ReactDOM                                                   from 'react-dom';
+import {FrozenGrid as Grid, Editors, Plugins, Data}   from 'slickgrid-es6';
+import Faker                                                      from 'faker';
+import _                                                          from 'lodash'
+import Dimensions                                                 from 'react-dimensions'
+import {
+    makeArray,
+    amountFormatter,
+    pipFormatter,
+    //imageFormatter,
+    //historicFormatter,
+    historicSyncFormatter,
+    dateFormatter,
+    totalFormatter,
+    healthFormatter,
+    countryFormatter,
+    rates,
+    morphRate
+} from './lib/utils';
+
+const checkboxSelector = new Plugins.CheckboxSelectColumn({
+    cssClass: "slick-cell-checkboxsel"
+});
+
+const options = {
+    rowHeight: 32,
+    editable: true,
+    enableAddRow: !true,
+    enableCellNavigation: true,
+    // asyncEditorLoading: false,
+    enableAsyncPostRender: true,
+    autoEdit: false,
+    forceFitColumns: true,
+    showHeaderRow: true,
+    headerRowHeight: 32,
+    explicitInitialization: true
+};
+
+const columnFilters = {};
+let healthValue = 0
+
+// data view
+const dv = new Data.DataView();
+dv.setFilter(item => {
+    let pass = true;
+
+    for (let key in item){
+        pass = pass && item.health >= healthValue
+        if (key in columnFilters && columnFilters[key].length && key !== 'health'){
+            pass = pass && String(item[key]).match(new RegExp(columnFilters[key], 'ig'));
+        }
+    }
+    return pass;
+});
+
+dv.getItemMetadata = index => {
+    const row = dv.getItem(index);
+    return row.type === 'BUY' ? {cssClasses: 'buy'} : {cssClasses: ''}
+}
+// end data view
+
+
+// column definitions
+const sortable = true;
+const columns = [
+    checkboxSelector.getColumnDefinition(),
+    {
+        id: 'type',
+        name: 'Side',
+        sortable,
+        field: 'type',
+        maxWidth: 120,
+    },
+    {id: 'counterparty', name: 'Counterparty', field: 'counterparty', minWidth: 200, maxWidth: 200, cssClass: 'zb-editable', editor: Editors.Text, sortable},
+    {id: 'currency',
+        name: 'GBP-nnn',
+        field: 'currency',
+        minWidth: 90,
+        maxWidth: 90,
+        sortable,
+        formatter: countryFormatter,
+    },
+    {
+        id: 'price',
+        name: 'Price',
+        field: 'price',
+        headerCssClass: 'amount',
+        cssClass: 'amount',
+        formatter: pipFormatter,
+        minWidth: 100,
+        maxWidth: 100
+    },
+    {
+        id: 'historic',
+        name: 'Historic',
+        field: 'historic',
+        rerenderOnResize: true,
+        //asyncPostRender: historicFormatter,
+        formatter: historicSyncFormatter,
+        // formatter: (a,b, value) => JSON.stringify(value),
+        // formatter: historicFormatter,
+        cssClass: 'full-size',
+        minWidth: 128,
+        maxWidth: 128
+    }, {
+        id: 'amount',
+        name: 'Amount',
+        field: 'amount',
+        headerCssClass: 'amount',
+        cssClass: 'amount zb-editable',
+        formatter: amountFormatter,
+        editor: Editors.Text,
+        minWidth: 100,
+        maxWidth: 100
+    },
+    {
+        id:'total',
+        name:'Total',
+        fieldName:'total',
+        headerCssClass: 'amount',
+        cssClass: 'amount',
+        formatter: totalFormatter,
+        sortable: false,
+        minWidth: 100,
+        maxWidth: 128
+    },
+    {
+        id: 'paymentDate',
+        sortable: true,
+        name: 'Execution',
+        field: 'paymentDate',
+        minWidth: 100,
+        maxWidth: 100,
+        cssClass: 'zb-editable amount',
+        headerCssClass: 'amount',
+        editor: Editors.Date,
+        options: {
+            date: {
+                dateFormat: 'd/m/Y', // see https://chmln.github.io/flatpickr/#options,
+                parseDate: input => {
+                    const split = input.split('/')
+                    return new Date(`${split[1]}-${split[0]}-${split[2]}`)
+                }
+            }
+        }
+    },
+    {id: 'health', name: 'Health', field: 'health', cssClass: 'is-hidden-mobile', headerCssClass: 'is-hidden-mobile', formatter: healthFormatter, sortable: true},
+];
+
+// fake data
+const data = dv.setItems(makeArray(300, id => {
+    const currency = _.sample(['USD','AUD','CAD','EUR','JPY','CHF']);
+    const data = {
+        id,
+        avatar: Faker.image.avatar(),
+        type: _.sample(['BUY','SELL']),
+        counterparty: Faker.company.companyName(),
+        health: _.random(0, 100),
+        currency,
+        amount: Faker.finance.amount(),
+        price: rates[currency],
+        paymentDate: dateFormatter(null, null, Faker.date.future())
+    };
+
+    data.historic = [data.price]
+    return data
+}));
+
+// filter renderer is a react component
+class Filter extends React.Component {
+
+    handleChange = ({target}) => {
+        const value = target.value.trim()
+        if (value.length){
+            this.props.columnFilters[this.props.columnId] = value;
+        }
+        else {
+            delete this.props.columnFilters[this.props.columnId]
+        }
+
+        this.props.dv.refresh()
+    }
+
+    render(){
+        return <input defaultValue={this.props.columnFilters[this.props.columnId]} type='text' className='editor-text' onChange={this.handleChange} />
+    }
+}
+
+// main!
+class Home extends React.Component {
+
+    constructor(props){
+        super(props)
+        this.handleResize = this.handleResize.bind(this)
+        this.rates = Object.keys(rates)
+        this.historic = this.rates.reduce((acc, cur) => {
+            acc[cur] = [rates[cur]]
+            return acc
+        }, {})
+    }
+
+    handleResize(){
+        this.gridInstance.setColumns(columns)
+    }
+
+    componentDidMount(){
+        const grid = this.gridInstance = new Grid(this.grid, dv, columns, options);
+        columns[7].formatter = columns[7].formatter.bind(this.gridInstance)
+
+        grid.setSelectionModel(new Plugins.RowSelectionModel({selectActiveRow: false}));
+        grid.registerPlugin(checkboxSelector);
+
+        const changeFilter = _.debounce(value => {
+            healthValue = value
+            dv.refresh()
+        }, 500)
+
+        grid.onHeaderRowCellRendered.subscribe((e, {node, column}) => {
+            if (['_checkbox_selector', 'historic', 'health'].indexOf(column.id) === -1){
+                ReactDOM.render(<Filter columnId={column.id} columnFilters={columnFilters} dv={dv} />, node);
+                node.classList.add('zb-editable');
+            }
+            else if (column.id === 'health') {
+                ReactDOM.render(<input className="range" defaultValue={healthValue} type="range" onChange={e => changeFilter(e.target.value)} />, node)
+            }
+        });
+
+        grid.onSort.subscribe(function(e, args) {
+            // args.multiColumnSort indicates whether or not this is a multi-column sort.
+            // If it is, args.sortCols will have an array of {sortCol:..., sortAsc:...} objects.
+            // If not, the sort column and direction will be in args.sortCol & args.sortAsc.
+
+            // We'll use a simple comparer function here.
+            const comparer = function(a, b) {
+                return (a[args.sortCol.field] > b[args.sortCol.field]) ? 1 : -1;
+            }
+
+            // Delegate the sorting to DataView.
+            // This will fire the change events and update the grid.
+            dv.sort(comparer, args.sortAsc);
+        });
+
+        dv.onRowCountChanged.subscribe(() => {
+            grid.updateRowCount();
+            grid.render();
+        });
+
+        grid.onCellChange.subscribe((e, {item}) => {
+            dv.updateItem(item.id, item)
+        })
+
+        dv.onRowsChanged.subscribe((e, {rows}) => {
+            grid.invalidateRows(rows);
+            grid.render();
+        });
+
+        grid.init();
+
+        window.addEventListener('resize', this.handleResize);
+
+        this.mutate();
+    }
+
+    // cellUpdate(id, item, column){
+    //   const idx = dv.getIdxById(id);
+    //   if (idx === undefined || id !== item.id) {
+    //     throw "Invalid or non-matching id";
+    //   }
+    //   dv.getItems()[idx] = item;
+    //
+    //   const columnsToUpdate = Array.from(column)
+    //   const colIndex = columnsToUpdate.map(this.instance.getColumnIndex);
+    //   const range = this.instance.getRenderedRange()
+    //
+    //   for (let row = range.top; row <= range.bottom; row++) {
+    //     for (let col = 0; col < columns.length; col++) {
+    //       this.instance.updateCell(row, col);
+    //
+    //     }
+    //   }
+    // }
+
+    mutate = () => {
+        const currency = _.sample(this.rates);
+        const price = morphRate(currency);
+        this.historic[currency].push(price);
+        this.historic[currency].length > 15 && this.historic[currency].unshift()
+        let hasUpdates = false
+        dv.getItems().forEach(item => {
+            if (item.currency === currency){
+                item.direction = price > item.price ? 'up' : 'down';
+                item.price = price;
+                item.historic = this.historic[currency];
+
+                if (!hasUpdates){
+                    hasUpdates = true
+                    dv.beginUpdate()
+                }
+                dv.updateItem(item.id, item);
+            }
+        })
+
+        hasUpdates && dv.endUpdate()
+        setTimeout(this.mutate, _.random(100, 1000))
+    }
+
+    componentWillUnmount(){
+        this.gridInstance.destroy();
+        window.removeEventListener('resize', this.handleResize);
+    }
+
+    render(){
+        return <div className="" style={{height: this.props.containerHeight + 'px'}}>
+            <div id='myGrid' className='slickgrid-container' ref={grid => this.grid = grid} />
+        </div>
+    }
+}
+
+export default Dimensions()(Home)
---
+++ src/lib/utils.js
@@ -0,0 +1,84 @@
+import numeral from 'numeral';
+import fecha from 'fecha';
+import 'numeral/min/locales.min';
+import _ from 'lodash';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Sparklines, SparklinesLine, SparklinesReferenceLine, SparklinesSpots } from 'react-sparklines'
+
+numeral.locale('en-gb');
+
+const makeArrayFactory = () =>{
+  function *helper(count, cb){
+    for (let i = 0; i < count; i++) yield cb(i)
+  }
+
+  return (length, indiceGenerator = index => index) => ([...helper(length, indiceGenerator)])
+}
+
+const amountFormatter = (r, c, value) => `<div class="${value < 500 ? 'green' : 'red'}">${numeral(value).format('$ 0,000.00')}</div>`
+const pipFormatter = (r, c, value, cd, dataContext) =>{
+  return `<div class='${dataContext.direction}'>${numeral(value).format('0.0000')}</div>`;
+}
+const imageFormatter = (r, c, value) => `<img src="${value}" />`;
+const dateFormatter = (r, c, value) => fecha.format(new Date(value), 'D/MM/YYYY');
+const totalFormatter = (r, c, value, cd, {price, amount}) => numeral(price*amount).format('$ 0,000.00')
+const healthFormatter = (r, c, value) => {
+  const className = value > 66 ? 'danger' : ''
+  return `<progress value="${value}" max="100" class="${className}">${value}</progress>`
+}
+const countryFormatter = (r, c, value) => {
+  const val = value.substr(0, 2).toLocaleLowerCase()
+  return `<span class="flag-icon flag-icon-${val}"></span> ${value}`
+}
+
+const style = { stroke: "black", fill: "none" }
+const dummy = document.createElement('div')
+const historicSyncFormatter = (r, c, value) => {
+  dummy.innerHTML = ''
+  ReactDOM.render(<Sparklines data={value} limit={15} width={128} height={30} margin={0}>
+    <SparklinesLine style={style} />
+    <SparklinesReferenceLine />
+    <SparklinesSpots />
+  </Sparklines>, dummy);
+  return dummy.innerHTML
+}
+
+const historicFormatter = (node, row, data) => {
+  ReactDOM.render(<Sparklines data={data.historic} limit={15} width={128} height={20} margin={0}><SparklinesLine style={style} /></Sparklines>, node[0]);
+}
+
+
+const makeArray = makeArrayFactory()
+
+const rates = {
+  "AUD": 1.7443,
+  "CAD": 1.7643,
+  "CHF": 1.2963,
+  "JPY": 146.4,
+  "USD": 1.2854,
+  "EUR": 1.1836
+}
+
+const morphRate = (symbol, noSave) =>{
+  const rate = rates[symbol];
+  const diff = rate / 100 * _.random(0.01, 0.25);
+  const newRate = _.random(0, 1) ? rate + diff : rate - diff;
+  noSave || (rates[symbol] = newRate)
+  return newRate
+}
+
+export {
+  makeArray,
+  amountFormatter,
+  imageFormatter,
+  dateFormatter,
+  pipFormatter,
+  totalFormatter,
+  historicFormatter,
+  historicSyncFormatter,
+  healthFormatter,
+  countryFormatter,
+  rates,
+  morphRate
+}
