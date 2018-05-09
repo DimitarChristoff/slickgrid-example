78253ce3dc2732f0d33c18794ce016142a1de1c9
DimitarChristoff <christoff@gmail.com>
2018-05-09 12:52:12 +0100

    proper build now

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -153,6 +153,19 @@ module.exports = {
               compact: true,
             },
           },
+          {
+              test: /\.less$/,
+              use: [{
+                  loader: require.resolve('style-loader')
+              }, {
+                  loader: require.resolve('css-loader')
+              }, {
+                  loader: require.resolve('less-loader'), options: {
+                      strictMath: true,
+                      noIeCompat: true
+                  }
+              }]
+          },
           // The notation here is somewhat confusing.
           // "postcss" loader applies autoprefixer to our CSS.
           // "css" loader resolves paths in CSS and adds assets as dependencies.
